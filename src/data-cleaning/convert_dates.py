e3729184b4a2e94f9ed5ffd3cfdae89db27b9be8
JoeM PC <joe.a.mcglynn@gmail.com>
2024-02-13 16:50:00 +0000

    Added conmvert date function and applied to data gathering

---
+++ src/data-cleaning/convert_dates.py
@@ -0,0 +1,12 @@
+## This is a function to conver dates for the form
+## XXX BC -> -XXX
+## AD XXX -> XXX
+
+def convert_date(date: str):
+    if 'BC' in date:
+        date = date.strip(' BC')
+        date = -int(date)
+    elif 'AD' in date:
+        date = date.strip('AD ')
+        date = int(date)
+    return(date)
--- src/data-cleaning/extract_data.py
+++ src/data-cleaning/extract_data.py
@@ -2,6 +2,7 @@
 
 import json
 import pandas as pd
+import convert_dates
 
 with open('data/raw/pleiades-places-latest.json', 'r', encoding='utf-8') as file:
     json_data = json.load(file)
@@ -18,8 +19,12 @@ for entry in settlement_data:
         if entry[1][0]['geometry'] != None: # This filters out data with missing coordinate values
             
             ##The Following help us deal with entries with missing dates
-            try: dates = entry[1][0]['properties']['snippet'].split(';')[1].split('-')
+            try: 
+                dates = entry[1][0]['properties']['snippet'].split(';')[1].split('-')
+                dates[0] = convert_dates.convert_date(dates[0])
+                dates[1] = convert_dates.convert_date(dates[1])
             except Exception as e: dates = [None, None]
+
 
             row = [entry[0], entry[1][0]['geometry']['coordinates'], dates[0], dates[1]]
             settlement_df_list.append(row)
@@ -29,12 +34,15 @@ settlement_df = pd.DataFrame(settlement_df_list, columns = ['id', 'coordinate', 
 
 road_df_list = []
 road_df = pd.DataFrame(columns = ['id', 'coordinates', 'Start_Date', 'End_Date'])
-for entry in settlement_data:
+for entry in road_data:
     if entry[1] != []:# This filters out data with missing feature values
         if entry[1][0]['geometry'] != None: # This filters out data with missing coordinate values
 
             ##The Following help us deal with entries with missing dates
-            try: dates = entry[1][0]['properties']['snippet'].split(';')[1].split('-')
+            try: 
+                dates = entry[1][0]['properties']['snippet'].split(';')[1].split('-')
+                dates[0] = convert_dates.convert_date(dates[0])
+                dates[1] = convert_dates.convert_date(dates[1])
             except Exception as e: dates = [None, None]
 
             row = [entry[0], entry[1][0]['geometry']['coordinates'], dates[0], dates[1]]
