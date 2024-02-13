849b3977cb6a4358075d4666fa3a84de4263cf91
JoeM PC <joe.a.mcglynn@gmail.com>
2024-02-13 16:13:55 +0000

    Data gathering and extraction Working!!

---
+++ src/data-cleaning/extract_data.py
@@ -0,0 +1,47 @@
+## This function will create datasets for settlement and road types
+
+import json
+import pandas as pd
+
+with open('data/raw/pleiades-places-latest.json', 'r', encoding='utf-8') as file:
+    json_data = json.load(file)
+
+data = json_data['@graph'] ## Data entries stores in the Value of the dictionary
+
+settlement_data = [(location['id'], location['features']) for location in data if location['placeTypes'] == ['settlement']] ## Extracts the features for settlement entries, We keep the ID so we can found out more information later
+
+road_data = [(location['id'], location['features']) for location in data if location['placeTypes'] == ['road']] ## Extracts the features for road entries
+
+settlement_df_list = [] ## We will create a list and convert to pd.df, this method takes considerably less time than appending df itself
+for entry in settlement_data:
+    if entry[1] != []:# This filters out data with missing feature values
+        if entry[1][0]['geometry'] != None: # This filters out data with missing coordinate values
+            
+            ##The Following help us deal with entries with missing dates
+            try: dates = entry[1][0]['properties']['snippet'].split(';')[1].split('-')
+            except Exception as e: dates = [None, None]
+
+            row = [entry[0], entry[1][0]['geometry']['coordinates'], dates[0], dates[1]]
+            settlement_df_list.append(row)
+
+settlement_df = pd.DataFrame(settlement_df_list, columns = ['id', 'coordinate', 'Start_Date', 'End_Date'])
+
+
+road_df_list = []
+road_df = pd.DataFrame(columns = ['id', 'coordinates', 'Start_Date', 'End_Date'])
+for entry in settlement_data:
+    if entry[1] != []:# This filters out data with missing feature values
+        if entry[1][0]['geometry'] != None: # This filters out data with missing coordinate values
+
+            ##The Following help us deal with entries with missing dates
+            try: dates = entry[1][0]['properties']['snippet'].split(';')[1].split('-')
+            except Exception as e: dates = [None, None]
+
+            row = [entry[0], entry[1][0]['geometry']['coordinates'], dates[0], dates[1]]
+            road_df_list.append(row)
+
+road_df = pd.DataFrame(road_df_list, columns = ['id', 'coordinates', 'Start_Date', 'End_Date'])
+
+settlement_df.to_csv(r'./data/derived/settlement_df.csv')
+road_df.to_csv(r'./data/derived/road_df.csv')
+
--- src/data-gathering/get_data.py
+++ src/data-gathering/get_data.py
@@ -1,16 +1,15 @@
 ### This file will download the data and create a json file in data/raw and 
-
 import urllib.request
 import gzip
 import json
 
 url = 'https://atlantides.org/downloads/pleiades/json/pleiades-places-latest.json.gz'  ## This url could change
-output_file = '../../data/raw/pleiades-places-latest.json'
+output_file = r'./data/raw/pleiades-places-latest.json'
 
 # Download the compressed JSON file
-urllib.request.urlretrieve(url, '../../data/raw/pleiades-places-latest.json.gz')
+urllib.request.urlretrieve(url, r'./data/raw/pleiades-places-latest.json.gz')
 
 # Decompress the file and save the JSON data
-with gzip.open('../../data/raw/pleiades-places-latest.json.gz', 'rb') as f_in:
+with gzip.open(r'./data/raw/pleiades-places-latest.json.gz', 'rb') as f_in:
     with open(output_file, 'wb') as f_out:
         f_out.write(f_in.read())
