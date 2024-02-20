dfc4d3f7f6c66032f02e13d9a09aa31a3f5ff476
JoeM PC <joe.a.mcglynn@gmail.com>
2024-02-20 14:58:22 +0000

    more work done

---
+++ reports/Roman_Article.md
@@ -0,0 +1,16 @@
+# The Rise and Fall of the Roman Empire
+
+The Roman Empire has cemented itself in time as one of the greatest civilizations ever created. It’s history, full of conquest, great leaders, and eventual decline. 
+
+The birth of the Roman Empire technically began in 27 BCE with Octavian, the adopted son of Julias Caesar, although the Roman Republic and Kingdom had been around since 753 BCE. Figure 1 shows how the Roman Republic/ Empire had already grown substantially by 27 BCE relative to the rest of the ancient world. This growth would continue as the Empire spread across most of western Europe and the Middle East until the height of the Empire was reached in 117 CE: shown in figure 2 by the combination of the blue and red dots. 
+
+Peace and prosperity would continue for almost a century, but things would start to turn after the death of the well-respected Emperor Marcus Aurelius in 180 CE. Control of the Empire would change hands many times before the eventual collapse. By the ‘Crisis of the Third Century’, Roman legions had begun infighting, and the Empire was continually attacked.
+
+With this decline, the ‘Fall of The Western Roman Empire’ eventually happened in 476 CE; this is often seen as the true end to the once great Roman Empire. Both figures show this demise; figure 1 shows how the relative number of Roman places had suffered massive decreases compared to the ancient places of that era. While figure 2 paints a more geographical image of this fall, showing in red Roman places that had been destroyed by 480 CE, many places survived but would have been lost to enemy tribes and Civilizations. 
+
+The story of the Roman Empire is perhaps one of the greatest in human history and will surely not be forgotten.
+
+
+![Figure 1](../outputs/figures/figure-1.svg)
+
+![Figure 2](../outputs/figures/figure-2.svg)
--- src/create_plots/make_figure-1.py
+++ src/create_plots/make_figure-1.py
@@ -1,59 +1,95 @@
-### This file will create the relavent data from the Pleiades_df.csv and create Figure-1
+### This file will create the relavent data from the Pleiades_df.csv and create Figure-2
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
 
-df = pd.read_csv(r'./data/derived/Pleiades_df.csv')
+df = pd.read_csv(r'data/derived/Pleiades_df.csv')
 df.loc[df['Description'].isna(), 'Description'] = 'Missing'
-df = df.dropna()
+df = df.dropna() ## We are using the full dataset for this plot so we want to remove data entries with missing values
+
 
 ## Here we are getting all data entries that have 'roman' in the title or
 ## 'a roman' in the discription, we take the article a to ensure the description references a roman thing as supposed to 'This Town would trade with the roman town nearby' 
+
 Roman_title = df[df['Title'].str.lower().str.contains('roman ')]
 Roman_decri = df[df['Description'].str.lower().str.contains('a roman ')]
+Roman_full = pd.merge(Roman_title, Roman_decri, how = 'outer') 
 
-roman_df = pd.merge(Roman_title, Roman_decri, how = 'outer')
+## Get the number of places for different dates
+no_places_300bc =len(df[(df['Start_Date']<= -300)& (df['End_Date']>=-300)])
+no_roman_places_300bc = len(Roman_full[(Roman_full['Start_Date']<= -300)& (Roman_full['End_Date']>=-300)])
+no_places_27bc =len(df[(df['Start_Date']<= -27)& (df['End_Date']>=-27)])
+no_roman_places_27bc = len(Roman_full[(Roman_full['Start_Date']<= -27)& (Roman_full['End_Date']>=-27)])
+no_places_117 =len(df[(df['Start_Date']<= 117)& (df['End_Date']>=117)])
+no_roman_places_117 = len(Roman_full[(Roman_full['Start_Date']<= 117)& (Roman_full['End_Date']>=117)])
+no_places_480 =len(df[(df['Start_Date']<= 480)& (df['End_Date']>=480)])
+no_roman_places_480 = len(Roman_full[(Roman_full['Start_Date']<= 480)& (Roman_full['End_Date']>=480)])
 
+### MAKE THE PLOT
 
-### Coordinates for plotting 
-lons_destroyed = roman_df[(roman_df['Start_Date']<=117) & (roman_df['End_Date']<=480)]['long']
-lats_destroyed = roman_df[(roman_df['Start_Date']<=117) & (roman_df['End_Date']<=480)]['lat']
-lons_survived = roman_df[(roman_df['Start_Date']<=117) & (roman_df['End_Date']>=480)]['long']
-lats_survived = roman_df[(roman_df['Start_Date']<=117) & (roman_df['End_Date']>=480)]['lat']
-lons_created = roman_df[(roman_df['Start_Date']>=117) & (roman_df['End_Date']>=480)]['long']
-lat_created = roman_df[(roman_df['Start_Date']>=117) & (roman_df['End_Date']>=480)]['lat']
+Category = ['300 BCE','27 BCE', '117 CE', '480 CE']
 
-
-######## THIS CREATES THE PLOT
-
-fig = plt.figure(figsize=(10, 6))
-ax = plt.axes(projection=ccrs.PlateCarree()) ## CAN change type of projection
-
-### This plots the map plot
+fig, ax1 = plt.subplots(figsize=(6, 6))
 
 
-ax.add_feature(cfeature.LAND, color = '#B7C6CF', zorder = 1)
-ax.add_feature(cfeature.BORDERS, color = 'white', zorder = 2)
+X = np.arange(4)
+ax2 = ax1.twinx()
 
 
-ax.scatter(lons_survived, lats_survived, s=5 , c ='#3EBCD2', linewidths = 0 ,transform=ccrs.PlateCarree(), zorder = 3, alpha = 0.5)
-ax.scatter(lons_destroyed, lats_destroyed, s=5,c ='#A81829', linewidths = 0 ,transform=ccrs.PlateCarree(), zorder = 4, alpha = 0.5)
-ax.scatter(lons_created, lat_created, s=5, c ='#EBB434', linewidths = 0 ,transform=ccrs.PlateCarree(), zorder = 5, alpha = 0.5)
-ax.axis('off')
-ax.set_extent([-10, 42, 57, 29], crs=ccrs.PlateCarree())
+ax1.bar(X-0.15, [no_places_300bc,no_places_27bc, no_places_117, no_places_480], color = '#3EBCD2', width = 0.3, zorder = 2)
+ax2.bar(X+0.15, [no_roman_places_300bc,no_roman_places_27bc, no_roman_places_117, no_roman_places_480], color = '#A81829', width = 0.3, zorder = 2)
+ax1.set_xlim(-1,4)
 
+
+ax1.grid(which="major", axis='y', color='#758D99', alpha=0.6, zorder=1)
+
+# Remove splines
+ax1.spines[['top','left', 'right']].set_visible(False)
+
+# Make left spine slightly thicker
+ax1.spines[['bottom']].set_linewidth(1.1)
+
+ax2.spines[['top','left', 'right']].set_visible(False)
+
+# Set custom labels for y-axis
+ax1.set_yticks([0,5000,10000,15000,20000])
+ax1.set_yticklabels([0,5,10,15,20], color = '#3EBCD2',
+                    ha = 'left',
+                    verticalalignment = 'bottom')
+
+## Hacky scaling - The labels are not %100 accurate but the difference is very small (just a difference of 2)
+## We scale like this so the two scales match up
+ax2_scale = (5000/no_places_117)*no_roman_places_117
+ax2.set_yticks([0,ax2_scale,ax2_scale*2,ax2_scale*3,ax2_scale*4])
+ax2.set_yticklabels([0,3,6,9,12], color = '#A81829',
+                    ha = 'right',
+                    verticalalignment='bottom')
+
+# Reformat y-axis tick labels
+ax1.yaxis.set_tick_params(labelleft=True,      # Put x-axis labels on left
+                         labelright=False,  # Set no x-axis labels on right
+                         left=False,       # Set no ticks on left
+                         labelsize=11,       # Set tick label size
+                         pad=-1)             # Lower tick labels a bit
+
+ax2.yaxis.set_tick_params(labelleft=False,      # Set no x-axis labels on left
+                         labelright=True,  # put x-axis labels on right
+                         right=False,       # Set no ticks on right
+                         labelsize=11,       # Set tick label size
+                         pad=-1)             # Lower tick labels a bit
+
+plt.xticks(X, Category) 
+ax1.xaxis.set_tick_params(bottom = False, labelsize = 11, pad = 1)
 
 # Add in line and tag
-ax.plot([0.12, .9],                 # Set width of line
-        [1.0, 1.0],                # Set height of line
+ax2.plot([0.12, .91],                  # Set width of line
+        [1.00, 1.00],                  # Set height of line
         transform=fig.transFigure,   # Set location relative to plot
         clip_on=False, 
         color='#E3120B', 
         linewidth=.6)
-ax.add_patch(plt.Rectangle((0.12,1.0),                # Set location of rectangle by lower left corder
+ax1.add_patch(plt.Rectangle((0.12,1.00),                 # Set location of rectangle by lower left corder
                            0.04,                       # Width of rectangle
                            -0.02,                      # Height of rectangle. Negative so it goes down.
                            facecolor='#E3120B', 
@@ -61,45 +97,14 @@ ax.add_patch(plt.Rectangle((0.12,1.0),                # Set location of rectangl
                            clip_on=False, 
                            linewidth = 0))
 
+# Add in title and axis titles
+ax1.text(x=0.12, y=.945, s="The Destruction of Roman Places", transform=fig.transFigure, ha='left', fontsize=13, weight=700, fontfamily = 'Sans')
+ax1.text(x=0.12, y=.87, s="Number of Ancient \'places\' \n'000",color ='#3EBCD2', transform=fig.transFigure, ha='left', weight = 400, fontsize=11, fontfamily = 'Sans')
+ax1.text(x=0.91, y=.87, s="Number of Roman \'places\'\n'00",color ='#A81829', transform=fig.transFigure, ha='right', weight = 400, fontsize=11, fontfamily = 'Sans')
 
-
-# Add in title and subtitle
-ax.text(x=0.12, y=.94, s="The Rise and Fall", transform=fig.transFigure, ha='left', fontsize=13, weight=700, fontfamily = 'Sans')
-ax.text(x=0.12, y=.905, s="Destruction and Creation of Roman \'Places\' in 117 CE compared to 480 CE", transform=fig.transFigure, ha='left', fontsize=11,weight = 400, fontfamily="Sans")
 
 # Set source text
-ax.text(x=0.12, y=.12, s="""Source: "Ancient roman places from Pleiades dataset" via pleiades.stoa.org""", transform=fig.transFigure, ha='left', fontsize=9, weight = 300, alpha=.75)
-
-## ADD legend
-
-ax.add_patch(plt.Rectangle((0.12,0.87),                # Set location of rectangle by lower left corder
-                           0.03,                       # Width of rectangle
-                           0.02,                      # Height of rectangle. Negative so it goes down.
-                           facecolor='#3EBCD2', 
-                           transform=fig.transFigure, 
-                           clip_on=False, 
-                           linewidth = 0))
-ax.add_patch(plt.Rectangle((0.24,0.87),                # Set location of rectangle by lower left corder
-                           0.03,                       # Width of rectangle
-                           0.02,                      # Height of rectangle. Negative so it goes down.
-                           facecolor='#A81829', 
-                           transform=fig.transFigure, 
-                           clip_on=False, 
-                           linewidth = 0))
-ax.add_patch(plt.Rectangle((0.37,0.87),                # Set location of rectangle by lower left corder
-                           0.03,                       # Width of rectangle
-                           0.02,                      # Height of rectangle. Negative so it goes down.
-                           facecolor='#EBB434', 
-                           transform=fig.transFigure, 
-                           clip_on=False, 
-                           linewidth = 0))
-
-ax.text(x=0.155, y=.87, s="Survived", transform=fig.transFigure, ha='left', fontsize=11,weight = 400, fontfamily="Sans")
-ax.text(x=0.275, y=.87, s="Destroyed", transform=fig.transFigure, ha='left', fontsize=11,weight = 400, fontfamily="Sans")
-ax.text(x=0.405, y=.87, s="Created", transform=fig.transFigure, ha='left', fontsize=11,weight = 400, fontfamily="Sans")
-
-
-
-
+ax1.text(x=0.12, y=.05, s="""Source: "Ancient places from Pleiades dataset" via pleiades.stoa.org""", transform=fig.transFigure, ha='left', fontsize=9, alpha=.75)
 
 plt.savefig(r'./outputs/figures/figure-1.svg', bbox_inches = 'tight', facecolor = 'white')
+plt.savefig(r'./outputs/figures/figure-1.pdf',dpi = 600, bbox_inches = 'tight', facecolor = 'white') ## Used for article demonstration
